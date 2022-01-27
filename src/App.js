95ad89acbf4dc26e46cae800b8a42ac9bedac89c
jolamb1 <jolamb1@umbc.edu>
2022-01-27 12:10:47 -0500

    Add item functionality and MUI

--- src/App.js
+++ src/App.js
@@ -2,35 +2,41 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import { listLocations, listItems } from './graphql/queries';
+import { listItems } from './graphql/queries';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import { createLocation as createLocationMutation, updateLocation as updateLocationMutation, deleteLocation as deleteLocationMutation } from './graphql/mutations';
 import { createItem as createItemMutation, updateItem as updateItemMutation, deleteItem as deleteItemMutation } from './graphql/mutations';
 
-const initialFormState = { name: '', unitprice: '' }
+const initialFormState = { name: '', unitPrice: '' }
 
 function App() {
 	
-	const [items, setItems] = useState([]);
+	const [itemList, setItemList] = useState([]);
 	const [formData, setFormData] = useState(initialFormState);
-	 useEffect(() => {
+	
+	useEffect(() => {
     getItems();
-  }, []);
+	}, []);
 	
 	async function getItems() {
-		const apiData = await API.graphql({ query: listItems});
-		setItems(apiData.data.listItems.items);
+		const apiData = await API.graphql({ query: listItems });
+		setItemList(apiData.data.listItems.items);
 	}
 	
-	async function createItem() {
+	async function createNewItem() {
 		if (!formData.name || !formData.unitPrice) return;
 		await API.graphql({ query: createItemMutation, variables: { input: formData } });
-    setItems([ ...items, formData ]);
-    setFormData(initialFormState);
+		setItemList([ ...itemList, formData ]);
+		setFormData(initialFormState);
 	}
 	
 	async function deleteItem({ id }) {
-    const newItemsArray = items.filter(item => item.id !== id);
-    setItems(newItemsArray);
+    const newItemsArray = itemList.filter(item => item.id !== id);
+    setItemList(newItemsArray);
     await API.graphql({ query: deleteItemMutation, variables: { input: { id } }});
   }
 	
@@ -45,25 +51,34 @@ function App() {
         value={formData.name}
       />
       <input
-        onChange={e => setFormData({ ...formData, 'unitprice': e.target.value})}
+        onChange={e => setFormData({ ...formData, 'unitPrice': e.target.value})}
         placeholder="Item unit price"
-        value={formData.unitprice}
+        value={formData.unitPrice}
       />
-      <button onClick={createItem()}>Create Item</button>
+      <button type="button" onClick={createNewItem} >Create Item</button>
       <div style={{marginBottom: 30}}>
-		<h1>List of Items</h1>
-        {
-          items.map(item => (
-            <div key={item.id || item.name}>
-              <h2>{item.name}</h2>
-              <p>{item.unitprice}</p>
-              <button onClick={() => deleteItem(item)}>Delete Item</button>
-            </div>
+        <h2>List of items</h2>
+		<Table size="small">
+			<TableHead>
+				<TableRow>
+					<TableCell>Item Name</TableCell>
+					<TableCell>Unit Price</TableCell>
+					<TableCell>Delete Item?</TableCell>
+				</TableRow>
+			</TableHead>
+			<TableBody>
+		{
+          itemList.map((note) => (
+            <TableRow key={note.id || note.name}>
+              <TableCell>{note.name}</TableCell>
+              <TableCell><p>{note.unitPrice}</p></TableCell>
+              <TableCell><button onClick={() => deleteItem(note)}>Delete Item</button></TableCell>
+            </TableRow>
+			
           ))
         }
-          <p>
-            Hey {user.username}, welcome to my app, with auth!
-          </p>
+			</TableBody>
+		</Table>
           <button onClick={signOut}>Sign out</button>
         </div>
 	</div>
